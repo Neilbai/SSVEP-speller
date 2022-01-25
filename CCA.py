9395a2e583b5d04ce91aea1f6f2ea07c5bda4dc0
Bai Songyun <uogdq@student.kit.edu>
2022-01-25 14:39:05 +0100

    version 1.4d

--- CCA.py
+++ CCA.py
@@ -4,10 +4,6 @@ The cca Programs integrated in the SSVEP speller
 """
 import time
 import numpy as np
-import os
-import glob
-import csv
-import matplotlib.pyplot as plt
 
 from canonicalca import calc_cca, read_easy
 
@@ -25,19 +21,21 @@ def CCA(file, savepath, SAMPLES_PER_SECOND, window_size_in_seconds):
     
     # clip eeg channels of used electrodes
     useful_channels = data[:, [0, 1, 2, 3, 4, 5, 6, 7]]
-    # frqeucies = [12, 12.25, 12.5, 11.75]
-    freq = [np.linspace(8,16,9),
-            np.linspace(8.25,16.25,9),
-            np.linspace(8.5,16.5,9),
-            np.linspace(8.75,16.75,9)]
-    freq = np.array(freq).transpose()
-    frqeucies = freq.flatten()
+    frqeucies = [12, 12.25, 12.5, 11.75]
+    # freq = [np.linspace(8,16,9),
+    #         np.linspace(8.25,16.25,9),
+    #         np.linspace(8.5,16.5,9),
+    #         np.linspace(8.75,16.75,9)]
+
+    # freq = np.array(freq).transpose()
+    # frqeucies = freq.flatten()
     CCA_result = []
     for frqeucy in frqeucies:
         
         # do the cca
-        xs, result_sum, result_max, score_sum, score_max, sin = calc_cca(useful_channels, SAMPLES_PER_SECOND, frqeucy, amount_of_sec=window_size_in_seconds)
-        CCA_result.append(xs)
-        tmp = max(CCA_result)
-        frq_index = CCA_result.index(tmp)
+        xs, result_sum, result_max, score_sum, score_max, sin = calc_cca(useful_channels, SAMPLES_PER_SECOND, frqeucy, window_size_in_seconds)
+        max_value = max(result_max)
+        CCA_result.append(max_value)
+    tmp = max(CCA_result)
+    frq_index = CCA_result.index(tmp)
     return frq_index
